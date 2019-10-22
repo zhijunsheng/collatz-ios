43749d5729a10417f04c36b9cd80d35f385aac11
xiaobao <mariuspuzhouli@gmail.com>
2019-10-22 20:29:34 +0800

    we connected  3 things

--- Collatz/ViewController.swift
+++ Collatz/ViewController.swift
@@ -1,25 +1,29 @@
-//
-//  ViewController.swift
-//  Collatz
-//
-//  Created by Donald Sheng on 2018-07-31.
-//  Copyright © 2018 GoldThumb Inc. All rights reserved.
-//
-
 import UIKit
-
 class ViewController: UIViewController {
-
+    @IBOutlet weak var itf: UITextField!
+    @IBOutlet weak var rtf: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func showResult(_ sender: UIButton) {
+        print("//  Created by Donald Sheng on 2018-07-31.")
+        print(itf.text)
+        
+        
+        /*
+         
+         let n = 23
+         
+         if n is even, the next number is n/2
+         if n is odd, the next number is 3 * n + 1
+         
+         this is the famous Collatz Conjecture
+         
+         23 => 70 => 35 => 106 => 53 => 160 => 80 => 40 => 20 => 10 => 5 => 16 => 8 => 4 => 2 => 1 => 4 => 2 => 1
+         
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+         
+         
+         */
     }
-
-
 }
-
