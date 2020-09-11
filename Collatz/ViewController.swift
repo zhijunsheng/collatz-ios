44691e3f269124762c147e28a3ad7df1f01197da
Felix Lo <felixthegreat@Felixs-MacBook-Pro.local>
2020-09-11 14:04:46 -0400

    we made the code work

--- Collatz/ViewController.swift
+++ Collatz/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number: UITextField!
+    @IBOutlet weak var result: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func start(_ sender: Any) {
+        guard let text = number.text, var num = Int(text) else {
+            return
+        }
+        
+        var resultString = ""
+        
+        resultString.append("\(num) → ")
+        
+        while num != 1 {
+            if num % 2 == 0 {
+                num = num/2
+            } else {
+                num = num * 3 + 1
+            }
+            resultString.append("\(num) → ")
+        }
+        result.text = resultString
+    }
 }
-
